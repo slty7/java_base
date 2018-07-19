9bdfa6b9ccc7747671c0794d070a55503393de5a
slty7 <315826333@qq.com>
2018-07-19 16:10:40 +0800

    更新1-10000000的质素

---
+++ src/com/stly7/eland/num_string/math/E_value.java
@@ -0,0 +1,14 @@
+package com.stly7.eland.num_string.math;
+/**
+ * 这个图是自然对数的计算方式。
+	借助Math的方法，把自然对数计算出来，看看经过自己计算的自然对数和Math.E的区别有多大
+ * @author Administrator
+ *
+ */
+public class E_value {
+	
+	public static void main(String[] args) {
+		double e = Math.E;
+		System.out.println(e);
+	}
+}
---
+++ src/com/stly7/eland/num_string/math/JudgeNum.java
@@ -0,0 +1,19 @@
+package com.stly7.eland.num_string.math;
+/**
+ * 统计找出一千万以内，一共有多少质数
+	质数概念: 只能被1和自己整除的数
+ * @author Administrator
+ *
+ */
+public class JudgeNum {
+	
+	public static void main(String[] args) {
+		int count = 3;
+		//知道1,2,3是素数
+		for (int i = 4; i < 10000000; i++) {
+			
+		}
+		
+		System.out.println("1-10000000所有的素数有" + count + "个");
+	}
+}

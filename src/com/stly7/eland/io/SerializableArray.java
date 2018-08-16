47ac9cebb44297d9b4e85252e982bf36c4908caf
slty7 <315826333@qq.com>
2018-08-16 17:12:11 +0800

    对象流 读写数组并且序列化

---
+++ src/com/stly7/eland/io/SerializableArray.java
@@ -0,0 +1,59 @@
+package com.stly7.eland.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.stly7.eland.io.item.Hero;
+
+/**
+ * 准备一个长度是10，类型是Hero的数组，使用10个Hero对象初始化该数组
+ * 然后把该数组序列化到一个文件heros.lol
+ * 接着使用ObjectInputStream 读取该文件，并转换为Hero数组，验证该数组中的内容，是否和序列化之前一样
+ * @author Administrator
+ *
+ */
+public class SerializableArray {
+	public static void main(String[] args) throws Exception {
+		seraliz();
+		read();
+	}
+	
+	public static void seraliz() {
+		Hero[] heros = new Hero[10];
+		File file = new File("D:/heros.lol");
+		
+		try (
+			//创建一个对象输出流
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+		) {
+			for (int i = 0; i < heros.length; i++) {
+				heros[i] = new Hero("英雄" + i, 10 + i);
+			}
+			oos.writeObject(heros);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public static void read() {
+		File file = new File("D:/heros.lol");
+		Hero[] heros = null;
+		try (
+				//创建一个对象输入流
+				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+			) {
+				heros = (Hero[])ois.readObject();
+				for (Hero hero : heros) {
+					System.out.println(hero.name + ": -->" + hero.hp);
+				}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+}

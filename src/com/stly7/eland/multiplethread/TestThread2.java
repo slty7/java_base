8d56450a7d20c475fe57d98c452068fe1bc3c52c
slty7 <315826333@qq.com>
2018-09-13 17:53:35 +0800

    线程的开启

---
+++ src/com/stly7/eland/multiplethread/TestThread2.java
@@ -0,0 +1,28 @@
+package com.stly7.eland.multiplethread;
+
+public class TestThread2 {
+	public static void main(String[] args) {
+		Hero gareen = new Hero();
+	    gareen.name = "¸ÇÂ×";
+	    gareen.hp = 616;
+	    gareen.damage = 50;
+
+	    Hero teemo = new Hero();
+	    teemo.name = "ÌáÄª";
+	    teemo.hp = 300;
+	    teemo.damage = 30;
+	     
+	    Hero bh = new Hero();
+	    bh.name = "ÉÍ½ğÁÔÈË";
+	    bh.hp = 500;
+	    bh.damage = 65;
+	     
+	    Hero leesin = new Hero();
+	    leesin.name = "Ã¤É®";
+	    leesin.hp = 455;
+	    leesin.damage = 80;
+	    
+	    new KillThread(gareen, teemo).start();
+	    new KillThread(bh, leesin).start();
+	}
+}
